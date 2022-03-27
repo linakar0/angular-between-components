7dc9b9366286643649f0bbdde70f73b6a65217e5
linakar0 <karolina.px@hotmail.com>
2022-03-27 10:59:12 +0200

    1 file modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { Comp1Component } from './components/comp1/comp1.component';
 import { Comp2Component } from './components/comp2/comp2.component';
+import { UserService } from './services/user.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, HelloComponent, Comp1Component, Comp2Component],
+  providers: [UserService],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
